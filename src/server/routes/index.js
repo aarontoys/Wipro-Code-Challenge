c929af14af8e90be0a7b3a2357fafd732a114902
Aaron Toys <aarontoys@gmail.com>
2016-06-20 15:06:53 -0600

    add route to query openweathermap.org api

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise')
+var apikey = process.env.APIKEY
 
-router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+router.get('/weather', function(req, res, next) {
+  var options = {
+    uri: 'http://api.openweathermap.org/data/2.5/forecast',
+    qs: {
+      lat: '39.695605',
+      lon: '-104.844136',
+      appid: apikey,
+      units: 'imperial'
+    } 
+  }
+
+  rp(options)
+  .then(function(result){
+    res.status(200).json({
+      status: 'success',
+      weather: JSON.parse(result)
+    })
+  })
+  .catch(function(err) {
+    return err;
+  })
 });
 
 module.exports = router;
